59186996acabe256d2995c921c56b4476a2db6d7
ferion0204 <dltjdgh@konkuk.ac.kr>
2021-12-22 22:25:16 +0900

    final setting by SH

--- js/map.js
+++ js/map.js
@@ -1,8 +1,34 @@
 
 var container = document.getElementById('map');
 var options = {
-	center: new kakao.maps.LatLng(37.50736, 127.034435),
+	center: new kakao.maps.LatLng(37.49939840960905,127.02901776936281),
 	level: 3
+	, 
 };
         
 var map = new kakao.maps.Map(container, options);
+
+var markerPosition  = new kakao.maps.LatLng(37.499398409609051, 127.02901776936281); 
+
+// 마커를 생성합니다
+var marker = new kakao.maps.Marker({
+    position: markerPosition
+});
+
+// 마커가 지도 위에 표시되도록 설정합니다
+marker.setMap(map);
+
+var iwContent = '<div  >비트캠프 본진</div>', // 인포윈도우에 표출될 내용으로 HTML 문자열이나 document element가 가능합니다
+    iwRemoveable = true; // removeable 속성을 ture 로 설정하면 인포윈도우를 닫을 수 있는 x버튼이 표시됩니다
+
+// 인포윈도우를 생성합니다
+var infowindow = new kakao.maps.InfoWindow({
+    content : iwContent,
+    removable : iwRemoveable
+});
+
+// 마커에 클릭이벤트를 등록합니다
+kakao.maps.event.addListener(marker, 'click', function() {
+      // 마커 위에 인포윈도우를 표시합니다
+      infowindow.open(map, marker);  
+});
