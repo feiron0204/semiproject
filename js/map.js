feb31f6d8f3ff1e38dc6b07741ca16c0d733b31d
sonchanwoo <chanchan1116@naver.com>
2021-12-22 11:03:40 +0900

    eject js by chanwoo

---
+++ js/map.js
@@ -0,0 +1,8 @@
+
+var container = document.getElementById('map');
+var options = {
+	center: new kakao.maps.LatLng(37.50736, 127.034435),
+	level: 3
+};
+        
+var map = new kakao.maps.Map(container, options);
