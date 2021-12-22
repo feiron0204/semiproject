830a9096cf2c5b1c656c99f5828a8508b4b81041
kimjinwook1 <87925272+kimjinwook1@users.noreply.github.com>
2021-12-22 18:15:10 +0900

    audio.js 오류 수정

--- js/audio.js
+++ js/audio.js
@@ -6,16 +6,18 @@ function changeVolume() {
     myAudios[index].playbackRate = window.innerHeight * 0.0014; //재생속도 0.25~4 <- 최대창 크기 시 재생속도 1로 맞춰놓음
   }
 }
+
 window.addEventListener("resize", changeVolume);
 
 function onlyOnePlay(e) {
-  for (let index in myAudios) {
-    if (!(myAudios[index] == e.target)) {
-      myAudios[index].pause();
+  for (let i = 0; i < myAudios.length; i++) {
+    let audio = myAudios[i];
+    if (audio != e.target) {
+      myAudios[i].pause();
     }
   }
 }
 
-for (let index in myAudios) {
-  myAudios[index].addEventListener("play", onlyOnePlay);
+for (let i = 0; i < myAudios.length; i++) {
+  myAudios[i].addEventListener("play", onlyOnePlay);
 }
