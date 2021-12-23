aa496fd1e8aac2915bab8cabe0f920f5906b57b9
kimjinwook1 <87925272+kimjinwook1@users.noreply.github.com>
2021-12-23 10:44:38 +0900

    audio.js Audio.html 수정

--- js/audio.js
+++ js/audio.js
@@ -2,8 +2,11 @@ let myAudios = document.querySelectorAll(".audio");
 
 function changeVolume() {
   for (let index in myAudios) {
-    myAudios[index].volume = window.innerWidth * 0.00065; //볼륨 : 0~1 <- 최대창 크기 시 볼륨 최대로 맞춰놓음
-    myAudios[index].playbackRate = window.innerHeight * 0.0014; //재생속도 0.25~4 <- 최대창 크기 시 재생속도 1로 맞춰놓음
+    myAudios[index].volume = window.innerWidth * (1 / screen.availWidth); //볼륨 : 0~1 <- 최대창 크기 시 볼륨 최대로 맞춰놓음
+    myAudios[index].playbackRate =
+      window.innerHeight * (1 / screen.availHeight); //재생속도 0.25~4 <- 최대창 크기 시 재생속도 1로 맞춰놓음
+    //screen.availWidth: 사용 가능한 스크린 값의 가로폭 값을 알아낼 수 있다.
+    //screen.availHeight: 사용 가능한 스크린 값의 높이 값을 알아낼 수 있다.
   }
 }
 
