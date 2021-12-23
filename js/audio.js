391b14fa875c7209a59d3800b0248b07a956051e
sonchanwoo <chanchan1116@naver.com>
2021-12-23 15:31:04 +0900

    audio refactoring

--- js/audio.js
+++ js/audio.js
@@ -1,22 +1,23 @@
 let myAudios = document.querySelectorAll(".audio");
 
-function changeVolume() {
+function changeVolumeAndPlaybackRate() {
   for (let index in myAudios) {
-    myAudios[index].volume = window.innerWidth * (1 / screen.availWidth); //볼륨 : 0~1 <- 최대창 크기 시 볼륨 최대로 맞춰놓음
-    myAudios[index].playbackRate =
+    let audio = myAudios[index];
+    audio.volume = window.innerWidth * (1 / screen.availWidth); //볼륨 : 0~1 <- 최대창 크기 시 볼륨 최대로 맞춰놓음
+    audio.playbackRate =
       window.innerHeight * (1 / screen.availHeight); //재생속도 0.25~4 <- 최대창 크기 시 재생속도 1로 맞춰놓음
     //screen.availWidth: 사용 가능한 스크린 값의 가로폭 값을 알아낼 수 있다.
     //screen.availHeight: 사용 가능한 스크린 값의 높이 값을 알아낼 수 있다.
   }
 }
 
-window.addEventListener("resize", changeVolume);
+window.addEventListener("resize", changeVolumeAndPlaybackRate);
 
 function onlyOnePlay(e) {
   for (let i = 0; i < myAudios.length; i++) {
     let audio = myAudios[i];
     if (audio != e.target) {
-      myAudios[i].pause();
+      audio.pause();
     }
   }
 }
