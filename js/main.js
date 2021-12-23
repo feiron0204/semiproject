f9046963f8ce386afd806468b6f7a0c900c37cf9
Jayden Minsu Cho <ncms0820>
2021-12-23 18:23:29 +0900

    #6 fixed js malfunction

--- js/main.js
+++ js/main.js
@@ -96,15 +96,3 @@
     },
   });
 })(jQuery);
-
-if (location.pathname == "/pages/Video.html") {
-  document.getElementById("first").innerHTML = "HTML Video";
-  document.getElementById("second").innerHTML = "Theory";
-  document.getElementById("third").innerHTML = "Example";
-  document.getElementById("fourth").innerHTML = "Video";
-} else if (location.pathname == "/pages/Audio.html") {
-  document.getElementById("first").innerHTML = "HTML Audio";
-  document.getElementById("second").innerHTML = "Theory";
-  document.getElementById("third").innerHTML = "Example";
-  document.getElementById("fourth").innerHTML = "Audio";
-}
---
+++ js/sub.js
@@ -0,0 +1,11 @@
+if (location.pathname == "/pages/Video.html") {
+  document.getElementById("first").innerHTML = "HTML Video";
+  document.getElementById("second").innerHTML = "Theory";
+  document.getElementById("third").innerHTML = "Example";
+  document.getElementById("fourth").innerHTML = "Video";
+} else if (location.pathname == "/pages/Audio.html") {
+  document.getElementById("first").innerHTML = "HTML Audio";
+  document.getElementById("second").innerHTML = "Theory";
+  document.getElementById("third").innerHTML = "Example";
+  document.getElementById("fourth").innerHTML = "Audio";
+}
